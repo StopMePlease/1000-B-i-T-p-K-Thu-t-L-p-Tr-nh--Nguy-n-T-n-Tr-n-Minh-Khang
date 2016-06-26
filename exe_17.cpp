a34c093e81f1000ab4184550bd87a9ed12a11f17
Duc Do <dotrananhduc@gmail.com>
2016-06-26 12:57:26 +0700

    exe_17

---
+++ exe_17.cpp
@@ -0,0 +1,31 @@
+/*
+	s(n) = x + x^2/2! + x^3/3! + ... + x^n/n!
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, x;
+	cout << "Input n: ";
+	cin >> n;
+	cout << "Input x: ";
+	cin >> x;
+
+	double sum = 0;
+	double temp, tempx;
+	for(int i = 1; i <= n; i++)
+	{
+		temp = 1;
+		tempx = 1;
+		for(int j = 1; j <= i; j++)
+		{
+			tempx *= x;
+			temp *= j;
+		}
+		sum += 1.0 * tempx / temp;
+	}
+	cout << "s(" << n << ") =  " << sum;
+	return 0;
+}

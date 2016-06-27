6c9ff674d9ccb945bc3f12d91027f1d9a8491dd9
Duc Do <dotrananhduc@gmail.com>
2016-06-27 19:45:34 +0700

    exe_25

---
+++ exe_25.cpp
@@ -0,0 +1,25 @@
+/*
+	Tinh tong tat ca uoc so chan cua so nguyen duong n
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+
+	int sum = 0;
+	
+	for(int i = 2; i <= n ; i += 2)
+	{
+		if(n % i == 0)
+		{
+			sum += i;
+		}
+	}
+	cout << "sum: " << sum;
+	return 0;
+}

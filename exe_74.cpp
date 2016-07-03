cc3089f9ba005296213e20f4faa9bde8c1dd1ee7
Duc Do <dotrananhduc@gmail.com>
2016-07-03 15:30:32 +0700

    exe_74

---
+++ exe_74.cpp
@@ -0,0 +1,37 @@
+/*
+	s(x, n) = 1 - x + x^3/3! - x^5/5! + ... + (-1)^(n+1).x^(2n+1)/(2n+1)!
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	int n, x;
+	cout << "Input n: ";
+	cin >> n;
+	cout << "Input x: ";
+	cin >> x;
+
+	double sum = 1 - x;
+	double temp, tempx;
+	for(int i = 1; i <= n; i++)
+	{
+		temp = 1;
+		tempx = 1;
+		for(int j = 1; j <= i * 2 + 1; j++)
+		{
+			tempx *= x;
+			temp *= j;
+		}
+		if((((i * 2) + 1) + 1 ) % 4 != 0)
+		{
+			tempx *= -1;
+		}
+		sum += 1.0 * tempx / temp;
+	}
+	
+	cout << "s(" << n << ") =  " << sum;
+	return 0;
+}
